fb65a865210b053b73a916ac7987da808fe35233
whowouldwin <leranetwork@gmail.com>
2025-03-10 16:50:42 +0300

    feat: implement makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -132,8 +132,17 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const positions = Object.values(lettersObject).flat();
+  if (positions.length === 0) return '';
+  const wordLength = Math.max(...positions) + 1;
+  const wordArray = new Array(wordLength).fill('');
+  Object.entries(lettersObject).forEach(([letter, indexes]) => {
+    indexes.forEach((index) => {
+      wordArray[index] = letter;
+    });
+  });
+  return wordArray.join('');
 }
 
 /**
