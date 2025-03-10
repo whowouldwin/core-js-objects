5edc2384b6108b9f7eadd1a70424b294358680c0
whowouldwin <leranetwork@gmail.com>
2025-03-10 15:59:18 +0300

    feat: implement mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,13 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return Object.fromEntries(
+    Object.entries(Object.assign({}, ...objects)).map(([key]) => [
+      key,
+      objects.reduce((sum, obj) => sum + (obj[key] || 0), 0),
+    ])
+  );
 }
 
 /**
