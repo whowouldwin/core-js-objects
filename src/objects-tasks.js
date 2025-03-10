a03ee43dfe2decac3fbcb2d76321342e1d94e49b
whowouldwin <leranetwork@gmail.com>
2025-03-10 17:02:52 +0300

    feat: implement fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -236,8 +236,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const object = JSON.parse(json);
+  Object.setPrototypeOf(object, proto);
+  return object;
 }
 
 /**
